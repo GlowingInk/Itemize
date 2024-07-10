59f82aaeda46f33161998e775d33bd7d3c1efc1b
Daniil Zao <idanix@list.ru>
2024-07-10 04:18:43 +0300

    Some preps for random resolver

---
+++ paper/item/src/main/java/ink/glowing/itemize/paper/item/RandomItemResolver.java
@@ -0,0 +1,26 @@
+package ink.glowing.itemize.paper.item;
+
+import ink.glowing.itemize.Itemize;
+import ink.glowing.itemize.util.WeightedPool;
+import net.kyori.adventure.key.Key;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Supplier;
+
+public class RandomItemResolver implements ItemResolver { // TODO
+    private static final Key KEY = Itemize.itemizeKey("random");
+
+    private WeightedPool<Supplier<ItemStack>> weightedPool;
+
+    @Override
+    public @Nullable ItemStack resolve(@NotNull String params) {
+        return null;
+    }
+
+    @Override
+    public @NotNull Key key() {
+        return KEY;
+    }
+}
